2104dfa0790a83350d8b83f9313dd9782d37e99c
steven <steven@nenyah.com>
2020-04-01 16:14:04 +0800

    init project

---
+++ app.js
@@ -0,0 +1,16 @@
+App({
+  onLaunch(options) {
+      console.log('App Launch', options);
+      console.log('getSystemInfoSync', dd.getSystemInfoSync());
+      console.log('SDKVersion', dd.SDKVersion);
+    },
+    onShow() {
+      this.globalData.sysInfo = dd.getSystemInfoSync()
+    },
+    onHide() {
+      console.log('App Hide');
+    },
+    globalData: {
+      sysInfo: {},
+    },
+});
