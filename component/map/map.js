baa5428faa03d4ebd52acb248135f1cded30cf1c
steven <steven@nenyah.com>
2020-04-05 17:30:20 +0800

    add map component

---
+++ component/map/map.js
@@ -0,0 +1,50 @@
+const markers = [{
+  iconPath: "/assets/images/location.png",
+  id: 10,
+  latitude: 30.279383,
+  longitude: 120.131441,
+  width: 30,
+  height: 30,
+  callout: {
+    content: 'callout',
+  },
+}];
+
+const includePoints = [{
+  latitude: 30.279383,
+  longitude: 120.141461,
+}];
+
+Component({
+  mixins: [],
+  data: {
+    scale: 14,
+    longitude: 120.131441,
+    latitude: 30.279383,
+    markers,
+    includePoints,
+    circles: [{
+      latitude: 30.279383,
+      longitude: 120.131441,
+      color: '#ff943e',
+      fillColor: '#ff943e',
+      radius: 80,
+    }],
+    controls: [{
+      id: 5,
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50,
+      },
+      clickable: true,
+    }],
+
+  },
+  props: {},
+  didMount() { },
+  didUpdate() { },
+  didUnmount() { },
+  methods: {},
+});
